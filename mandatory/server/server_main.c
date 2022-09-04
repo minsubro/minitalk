441c581fba5fe19299330c12b57df482db57031c
Minsu Kang <minsukan@c5r1s4.42seoul.kr>
2022-09-04 18:49:05 +0900

    9/4 e

--- mandatory/server/server_main.c
+++ mandatory/server/server_main.c
@@ -6,28 +6,34 @@
 /*   By: minsukan <minsukan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/03 16:10:54 by minsukan          #+#    #+#             */
-/*   Updated: 2022/09/04 16:26:58 by minsukan         ###   ########.fr       */
+/*   Updated: 2022/09/04 18:36:28 by minsukan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sever.h"
 
-int	get_sig1(char *c)
-{ㅣ
-	
+void	get_sig1(int i)
+{
+	ft_printf("sig1");
+}
+
+void	get_sig2(int i)
+{
+	ft_printf("sig2");
 }
 
 int main()
 {
-	char	*str;
-	char	c;
-	int cnt;
+	//char	*str;
+	//char	c;
+	//int cnt;
 
 	ft_printf("%d\n", getpid());
 	while (1)
 	{
-		signal(10, get_sig1(&c));
-		
+		signal(10, 1);
+		signal(12, 0);
+		pause();
 	}
 	
 }
